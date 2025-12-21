8c21da19009dcd7d3fc12dc07e273603aceb8a50
Subham Mondal <subham.mondal.23@aot.edu.in>
2025-12-21 12:52:42 +0530

    Enhance landing page components: update Awards, Education, Hero, Pricing, and Stats sections with detailed content and images

--- frontend/src/Landing_Page/Home/Education.js
+++ frontend/src/Landing_Page/Home/Education.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React from "react";
 
 function Education() {
-    return ( 
-        <h1>Education</h1>
-     );
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-6">
+          <img src="media/images/education.svg"></img>
+        </div>
+        <div className="col-6 mt-5">
+          <h1 className="fs-4 mb-3">Free and open market education</h1>
+          <p>
+            Varsity, the largest online stock market education book in the world
+            covering everything from the basics to advanced trading.
+          </p>
+          <a href="" style={{ textDecoration: "none" }}>
+            Try Kite demo{" "}
+            <i class="fa fa-long-arrow-right" aria-hidden="true"></i>
+          </a>
+          <p className="mt-5">
+            TradingQ&A, the most active trading and investment community in
+            India for all your market related queries.
+          </p>
+          <a href="" style={{ textDecoration: "none" }}>
+            Try Kite demo{" "}
+            <i class="fa fa-long-arrow-right" aria-hidden="true"></i>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Education;
--- frontend/src/Landing_Page/Home/Pricing.js
+++ frontend/src/Landing_Page/Home/Pricing.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import React from "react";
 
 function Pricing() {
-    return ( 
-        <h1>Pricing</h1>
-     );
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-4">
+          <h1 className="fs-3 mb-3">Unbeatable pricing</h1>
+          <p>
+            We pioneered the concept of discount broking and price transparency
+            in India. Flat fees and no hidden charges.
+          </p>
+          <a href="" style={{ textDecoration: "none" }}>
+            See Pricing{" "}
+            <i class="fa fa-long-arrow-right" aria-hidden="true"></i>
+          </a>
+        </div>
+        <div className="col-2"></div>
+        <div className="col-6 mb-3">
+          <div className="row text-center p-3">
+            <div className="col-6 mb-5 border">
+              <h1 className="p-3">₹0</h1>
+              <p>
+                Free equity delivery and
+                <br />
+                direct mutual funds
+              </p>
+            </div>
+            <div className="col-6 mb-5 border">
+              <h1 className="p-3">₹20</h1>
+              <p>Intraday and F&O</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Pricing;
