081ff759e2a03f5276ac7af3faaa432869db0181
Subham Mondal <subham.mondal.23@aot.edu.in>
2025-12-27 20:22:10 +0530

    feat: update server configuration; set default PORT and add health check endpoint

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,7 +4,7 @@ import { connectionDB } from "./db/db-connection.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT  || 8000;
 
 connectionDB()
   .then(() => {
@@ -15,3 +15,8 @@ connectionDB()
   .catch((error) => {
     console.log("Error connecting to DB", error);
   });
+
+
+app.get("/", (req, res)=>{
+  res.status(200).json({ message: "Backend is running!"})
+})
